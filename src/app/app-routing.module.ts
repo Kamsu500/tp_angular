0ae90d3033f291495e4363bbaeb5ef2cbe900f15
Kamsu10 <kamsudylane@gmail.com>
2022-12-09 12:31:16 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
+import { ProfileComponent } from './component/profile/profile.component';
+import { CartComponent } from './component/cart/cart.component';
+import { MyshopComponent } from './component/myshop/myshop.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'myshop',component:MyshopComponent },
+  { path:'',component:MyshopComponent },
+  { path:'cart',component:CartComponent},
+  { path:'profile',component:ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { MyshopComponent } from './component/myshop/myshop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CartComponent,
+    ProfileComponent,
+    MyshopComponent
   ],
   imports: [
     BrowserModule,
